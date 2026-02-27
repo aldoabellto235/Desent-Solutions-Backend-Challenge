36f639dde8046dc4b1e290722d47e110e51bb406
Aldo <aldo.abellto14@gmail.com>
2026-02-27 09:28:45 +0700

    fix: echo raw body bytes to preserve key order

--- internal/interfaces/http/handlers/echo_handler.go
+++ internal/interfaces/http/handlers/echo_handler.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -13,9 +14,9 @@ func NewEchoHandler() *EchoHandler {
 }
 
 func (h *EchoHandler) Echo(c echo.Context) error {
-	var body interface{}
-	if err := c.Bind(&body); err != nil {
-		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid JSON body"})
+	body, err := io.ReadAll(c.Request().Body)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Failed to read body"})
 	}
-	return c.JSON(http.StatusOK, body)
+	return c.Blob(http.StatusOK, "application/json", body)
 }
